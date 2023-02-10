caa3badc8e2f747ab526034048359794f96f2d3d
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-02-10 03:13:54 +0300

    Introduction to Python

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+box_number = 104
+
+# Print Bufflone Address: Apollo Road
+# Datatypes in Python
+# String formatting in Python
+
+# if/else, loops, functions
+
+print("P.O. Box {}, Apollo Road".format(box_number))
